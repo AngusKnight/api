dbdf47dd3dbb4eff5bd934436f827b9f267704c1
Scott McKenzie <scott.mckenzie@angusknight.com.au>
2019-10-11 14:02:04 +1100

    Configure container timezone.

--- dockerfs/usr/local/bin/init.sh
+++ dockerfs/usr/local/bin/init.sh
@@ -16,6 +16,9 @@ cat /etc/motd
 # Get environment variables to show up in SSH session
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
+# configure timezone
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
 # starting sshd process
 sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
 /usr/sbin/sshd
