62a13ac070de1bcbf790cdbfeaed670b4fa7cbc2
DESKTOP-VB5GUSP\Phill <borgosity@gmail.com>
2020-02-15 22:39:38 +1100

    test mail service in feedback message hook

--- public/extensions/custom/hooks/messages/hooks.php
+++ public/extensions/custom/hooks/messages/hooks.php
@@ -1,10 +1,23 @@
 <?php
 
+use Directus\Services\MailService;
+
 return [
     'actions' => [
         'item.create.feedback_messages' => function ($data) {
-          $content = 'New feedbackk message has arrived: ' . $data['message'];
-          notify('borgosity@gmail.com', 'New Feedback Message', $content);
+            $email[] = [
+                'to' => 'borgosity@gmail.com',
+                'subject' => 'New Feedback Message', 
+                'body' => '<p>A new message has arrive from {{name}} their email address is {{email}}.</p> <br> <p>{{message}}</p>',
+                'type' => 'html',
+                'data' => [ 
+                    'name' => $data['name'],
+                    'email' => $data['email'],
+                    'message' => $data['message']
+                ]
+            ]; 
+            $mailService = new MailService($this->container);
+            $mailService->send($email);
         }
       ]
 ];
